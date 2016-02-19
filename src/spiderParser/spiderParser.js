7065f4830db9d2478b26f02d41cd874958958ac4
krumdine <turonjeremy@gmail.com>
2016-02-19 18:37:05 +0100

    Prise en compte de l'endianness du fichier spider

--- src/spiderParser/spiderParser.js
+++ src/spiderParser/spiderParser.js
@@ -1,5 +1,5 @@
-var sizeFloat = 4 ;
-var defaultHeaderSize = 256 ;
+const sizeFloat = 4 ;
+const defaultHeaderSize = 256 ;
 
 "use strict" ;
 
@@ -9,80 +9,91 @@ var defaultHeaderSize = 256 ;
  * @param {function} callback The function to execute after reading the file, it take two parameters :
  *		- imagePix : the pixels of the image(s), it is a 2d array if the type is 'image', if the type is 'stack' it is a 3d array.
  *		- type : the type of the file.
+ * @param {} endianness Define the endianness, big endian if the value is undefined or flase, little indian otherwise.
  */
-function readSpiderFile(file, type, callback) {
-	var reader = new FileReader();
+function readSpiderFile(file, type, callback, endianness) {
+	try {
+		var reader = new FileReader();
 
-	reader.onload = function(e) {
-		var contents = e.target.result;
-		var imagePix = getContents(contents, type) ;
-		callback(imagePix, type)
-	};
+		reader.onload = function(e) {
+			var contents = e.target.result;
+			var imagePix = getContents(contents, type, endianness) ;
+			callback(imagePix, type)
+		};
 
-	reader.readAsArrayBuffer(file);
+		reader.readAsArrayBuffer(file);
+	} catch(e) {
+		alert("Impossible de lire le fichier.") ;
+	}
 }
 
 /**
  * @param {File} Contents A SPIDER file content to extract the pixels from.
  * @param {string} type Type of the file, 'image' if the file describe a single image, 'stack' if it describes a stack.
+ * @param {} endianness Define the endianness, big endian if the value is undefined or flase, little indian otherwise.
  * @return The pixels of the image(s).
  */
-function getContents(contents, type) {
-	var pos = 0 ;
-	var header = new DataView(contents, pos, defaultHeaderSize * sizeFloat) ;
-	pos = defaultHeaderSize * sizeFloat;
-	var fNsam = header.getFloat32(11 * sizeFloat) ;
-	var fLabrec = header.getFloat32(12 * sizeFloat) ;
-	var headerSize = fNsam * fLabrec;
-
-	//calculate the true header size
-	if(headerSize != defaultHeaderSize) {
-		pos = 0 ;
-		header = new DataView(contents, pos, headerSize * sizeFloat) ;
-		pos = headerSize * sizeFloat ;
-	}
-
-	var ncol = header.getFloat32(11 * sizeFloat) ; //number of pixels per row
-	var nrow = header.getFloat32(1 * sizeFloat) ;  //number of rows
-
-	if(type === 'image') {
-		image = new DataView(contents, pos, nrow * ncol * sizeFloat) ;
-
-		var res = new Array(nrow) ;
-		pos = 0 ;
-
-		for(var i = 0 ; i < nrow ; i++) {
-			res[i] = new Array(ncol) ;
-			for(var j = 0 ; j < ncol ; j++) {
-				res[i][j] = image.getFloat32(pos * sizeFloat ) ;
-				pos++ ;
-			}
-		}
-		return  res ;
-	}
-	else if(type === 'stack') {
-		var nslice = header.getFloat32(0 * sizeFloat) ;
-
-		stack = new DataView(contents, pos, nslice * nrow * ncol * sizeFloat) ;
-
-		var res = new Array(nslice) ;
+function getContents(contents, type, endianness) {
+	try {
 		var pos = 0 ;
+		var header = new DataView(contents, pos, defaultHeaderSize * sizeFloat) ;
+		pos = defaultHeaderSize * sizeFloat;
+		var fNsam = header.getFloat32(11 * sizeFloat, endianness) ;
+		var fLabrec = header.getFloat32(12 * sizeFloat, endianness) ;
+		var headerSize = fNsam * fLabrec;
 
-		for(var k = 0 ; k < nslice ; k++) {
-			res[k] = new Array(nrow) ;
+		//calculate the true header size
+		if(headerSize != defaultHeaderSize) {
+			pos = 0 ;
+			header = new DataView(contents, pos, headerSize * sizeFloat) ;
+			pos = headerSize * sizeFloat ;
+		}
+
+		var ncol = header.getFloat32(11 * sizeFloat, endianness) ; //number of pixels per row
+		var nrow = header.getFloat32(1 * sizeFloat, endianness) ;  //number of rows
+
+		if(type === 'image') {
+			image = new DataView(contents, pos, nrow * ncol * sizeFloat) ;
+
+			var res = new Array(nrow) ;
+			pos = 0 ;
+
 			for(var i = 0 ; i < nrow ; i++) {
-				res[k][i] = new Array(ncol) ;
+				res[i] = new Array(ncol) ;
 				for(var j = 0 ; j < ncol ; j++) {
-					res[k][i][j] = stack.getFloat32(pos * sizeFloat ) ;
+					res[i][j] = image.getFloat32(pos * sizeFloat, endianness ) ;
 					pos++ ;
 				}
 			}
+			return  res ;
 		}
-		return res ;
-	}
-	else {
-		console.log("unknown file type") ;
-		fileContents = undefined ;
+		else if(type === 'stack') {
+			var nslice = header.getFloat32(0 * sizeFloat, endianness) ;
+
+			stack = new DataView(contents, pos, nslice * nrow * ncol * sizeFloat) ;
+
+			var res = new Array(nslice) ;
+			var pos = 0 ;
+
+			for(var k = 0 ; k < nslice ; k++) {
+				res[k] = new Array(nrow) ;
+				for(var i = 0 ; i < nrow ; i++) {
+					res[k][i] = new Array(ncol) ;
+					for(var j = 0 ; j < ncol ; j++) {
+						res[k][i][j] = stack.getFloat32(pos * sizeFloat, endianness) ;
+						pos++ ;
+					}
+				}
+			}
+			return res ;
+		}
+		else {
+			console.log("unknown file type") ;
+			fileContents = undefined ;
+		}
+	}catch(e) {
+		alert("Impossible d'ouvrir le fichier.") ;
+		console.error(e.message) ;
 	}
 }
 
